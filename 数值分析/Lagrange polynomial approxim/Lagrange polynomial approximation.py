32b267128a6707e959dc09a9c1c5a132c7f0256f
zhou weijie <109130112+zhoudaren2021@users.noreply.github.com>
2023-12-24 18:29:55 +0800

    Add files via upload

---
+++ 数值分析/Lagrange polynomial approxim/Lagrange polynomial approximation.py
@@ -0,0 +1,22 @@
+import numpy as np
+import math
+def Lagrange_polynomial_approximation(matrix, x):
+    ak = np.ones(len(matrix))
+    bk = np.ones(len(matrix))
+    Lk = np.zeros(len(matrix))
+    Px = 0
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            if j == i:
+                continue
+            else:
+                ak[i] = ak[i] * (matrix[i][0] - matrix[j][0])
+                bk[i] = bk[i] * (x - matrix[j][0])
+        Lk[i] = bk[i] / ak[i]
+        Px += Lk[i] *  matrix[i][1]
+    return Px
+def main():
+    matrix = [[1, 0.1924], [1.05, 0.2414], [1.1, 0.2933], [1.15, 0.3492]]
+    print(Lagrange_polynomial_approximation(matrix, 1.09))
+    print(np.abs(Lagrange_polynomial_approximation(matrix, 1.09) - math.log10(math.tan(1.09))))
+main()
